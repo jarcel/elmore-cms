45d6195e24d320f313242cda58242a86842845d4
jarcel <jeff@jacapps.com>
2025-02-07 15:32:53 -0500

    Customized login screen

---
+++ src/admin/app.ts
@@ -0,0 +1,39 @@
+export default {
+  config: {
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pt-BR',
+      // 'pt',
+      // 'ru',
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      // 'zh-Hans',
+      // 'zh',
+    ],
+    tutorials: false,
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "",
+        "Auth.form.welcome.subtitle": "Log in to your account",
+      },
+    }
+  },
+  bootstrap() { },
+}
